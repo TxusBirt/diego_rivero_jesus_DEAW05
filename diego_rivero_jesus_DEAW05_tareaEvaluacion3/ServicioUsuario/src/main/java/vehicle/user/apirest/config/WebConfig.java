ecbeafef510eb5b08a8df903563883ad0f1d90f1
TxusBirt <jdiego@birt.eus>
2024-05-27 15:36:31 +0200

    app funcional

--- diego_rivero_jesus_DEAW05_tareaEvaluacion3/ServicioUsuario/src/main/java/vehicle/user/apirest/config/WebConfig.java
+++ diego_rivero_jesus_DEAW05_tareaEvaluacion3/ServicioUsuario/src/main/java/vehicle/user/apirest/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Establece el patrón de URL se permite
-                .allowedOrigins("http://localhost:4200") // Permite solicitudes desde ese origen
+                .allowedOrigins("http://localhost:8082") // Permite solicitudes desde ese origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permite los métodos HTTP especificados
                 .allowCredentials(true); // Permite enviar credenciales (cookies) en la solicitud
     }
